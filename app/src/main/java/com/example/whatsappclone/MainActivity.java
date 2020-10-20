70a9c8a96bdc13081fe6ae6b122e49018a5da2a8
wajihasiddiqui <56477014+wajihasiddiqui@users.noreply.github.com>
2020-10-20 15:01:25 +0500

    Top Menu

--- app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -1,15 +1,105 @@
 
 package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tablayout;
+    ViewPager viewPager;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tablayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        setUpWithViewPager(viewPager);
+        tablayout.setupWithViewPager(viewPager);
     }
+
+    private void setUpWithViewPager(ViewPager viewPager){
+
+        MainActivity.SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ChatFragment(),"CHATS");
+        adapter.addFragment(new StatusFragment(),"STATUS");
+        adapter.addFragment(new CallFragment(),"CALLS");
+        viewPager.setAdapter(adapter);
+    }
+
+    private static class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private final ArrayList<Fragment> fragmentList = new ArrayList();
+        private final ArrayList<String> fragmentTitle = new ArrayList();
+
+        public SectionsPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+
+            fragmentList.add(fragment);
+            fragmentTitle.add(title);
+        }
+
+        public CharSequence getPageTitle(int position){
+
+            return fragmentTitle.get(position);
+        }
+    }
+
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.menu_search:
+                Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.menu_more:
+                Toast.makeText(this, "main", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
