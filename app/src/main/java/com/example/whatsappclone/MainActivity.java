f6abe4a617841da7d1589435d7b6f2d7a734303d
wajihasiddiqui <56477014+wajihasiddiqui@users.noreply.github.com>
2020-10-21 14:10:02 +0500

    Floating Button

--- app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -9,33 +9,63 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageSwitcher;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
+
+import static com.example.whatsappclone.R.id.fab_action;
 
 public class MainActivity extends AppCompatActivity {
 
     TabLayout tablayout;
     ViewPager viewPager;
     Toolbar toolbar;
+    FloatingActionButton fabaction;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fabaction = findViewById(R.id.fab_action);
         tablayout = findViewById(R.id.tablayout);
         viewPager = findViewById(R.id.viewpager);
         toolbar = findViewById(R.id.toolbar);
 
+
         setSupportActionBar(toolbar);
         setUpWithViewPager(viewPager);
         tablayout.setupWithViewPager(viewPager);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                ChangeFabIcon(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     private void setUpWithViewPager(ViewPager viewPager){
@@ -81,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
+        getMenuInflater().inflate(R.menu.main_menu ,menu);
         return super.onCreateOptionsMenu(menu);
     }
     @Override
@@ -100,6 +130,33 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void ChangeFabIcon(final int index){
+
+
+        fabaction.hide();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                switch (index){
+                    case 0:
+                        //fabaction.setImageDrawable(getDrawable(R.drawable.ic_baseline_search_24));
+                        fabaction.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_message_24));
+                        break;
+                    case 1:
+                        fabaction.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_photo_camera_24));
+                        break;
+                    case 2:
+                        fabaction.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_call_24));
+                        break;
+                }
+                fabaction.show();
+            }
+        },400);
     }
 
 }
