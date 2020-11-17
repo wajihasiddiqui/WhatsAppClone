07fa13036578a196fa03cc9c8697f2f1a5c2e9ea
wajihasiddiqui <56477014+wajihasiddiqui@users.noreply.github.com>
2020-11-17 21:24:46 +0500

    Bottom Sheet dialog pick profile picture

--- app/src/main/java/com/example/whatsappclone/profile/ProfileActivity.java
+++ app/src/main/java/com/example/whatsappclone/profile/ProfileActivity.java
@@ -3,17 +3,26 @@ package com.example.whatsappclone.profile;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 
 import com.example.whatsappclone.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    FloatingActionButton fabcamera;
+    private BottomSheetDialog bottomsheetdialog;
 
 
     @Override
@@ -22,8 +31,45 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         toolbar = findViewById(R.id.toolbar);
+        fabcamera = findViewById(R.id.fab_camera);
 
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+
+
+        initActionClick();
+    }
+
+    private void initActionClick() {
+        fabcamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheetpickPhoto();
+
+            }
+        });
+
+    }
+
+    private void showBottomSheetpickPhoto() {
+
+        @SuppressLint("InflateParams") View view = getLayoutInflater().inflate(R.layout.bottom_sheet_pick,null);
+        bottomsheetdialog = new BottomSheetDialog(this);
+
+        bottomsheetdialog.setContentView(view);
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP) {
+            Objects.requireNonNull(bottomsheetdialog.getWindow()).addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        bottomsheetdialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                bottomsheetdialog = null;
+            }
+        });
+        bottomsheetdialog.show();
+        
     }
 }
