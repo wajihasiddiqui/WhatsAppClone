99aef09a8c56b065eb67b9bf28bb730b6b3367a4
wajihasiddiqui <56477014+wajihasiddiqui@users.noreply.github.com>
2020-11-19 11:45:49 +0500

    Pick photo from gallery

--- app/src/main/java/com/example/whatsappclone/profile/ProfileActivity.java
+++ app/src/main/java/com/example/whatsappclone/profile/ProfileActivity.java
@@ -1,21 +1,30 @@
 package com.example.whatsappclone.profile;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 import com.example.whatsappclone.R;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.net.URI;
 import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -23,6 +32,11 @@ public class ProfileActivity extends AppCompatActivity {
     Toolbar toolbar;
     FloatingActionButton fabcamera;
     private BottomSheetDialog bottomsheetdialog;
+
+    private int Image_gallry_request = 111;
+    private Uri imageUrl;
+
+    ImageView img_profile;
 
 
     @Override
@@ -32,6 +46,8 @@ public class ProfileActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         fabcamera = findViewById(R.id.fab_camera);
+
+        img_profile = findViewById(R.id.img_profile);
 
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
@@ -55,6 +71,27 @@ public class ProfileActivity extends AppCompatActivity {
     private void showBottomSheetpickPhoto() {
 
         @SuppressLint("InflateParams") View view = getLayoutInflater().inflate(R.layout.bottom_sheet_pick,null);
+
+
+        ((View)view.findViewById(R.id.In_gallery)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opengallery();
+                bottomsheetdialog.dismiss();
+
+            }
+        });
+
+        ((View)view.findViewById(R.id.In_camera)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "camera",Toast.LENGTH_SHORT);
+                bottomsheetdialog.dismiss();
+
+            }
+        });
+
+
         bottomsheetdialog = new BottomSheetDialog(this);
 
         bottomsheetdialog.setContentView(view);
@@ -71,5 +108,34 @@ public class ProfileActivity extends AppCompatActivity {
         });
         bottomsheetdialog.show();
         
+    }
+
+    private void opengallery(){
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent.createChooser(intent,"select image"),Image_gallry_request);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == Image_gallry_request && resultCode == RESULT_OK && data != null && data.getData() != null){
+
+            imageUrl = data.getData();
+
+            try
+            {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imageUrl);
+                img_profile.setImageBitmap(bitmap);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
